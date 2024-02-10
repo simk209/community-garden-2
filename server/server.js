ffbac2dd7fe1cf797f1540fc2a31f6fd64fe1d20
Simran Kaur <simran00kk@gmail.com>
2024-02-09 20:46:51 -0500

    added catch all error handlers

--- server/server.js
+++ server/server.js
@@ -27,3 +27,27 @@ app.post('/login', userController.login, (req,res)=>{
 app.post('/signup', userController.signup, (req,res)=>{
     return res.sendStatus(200)
 })
+
+// catch-all route handler for any requests to an unknown route
+app.use('*', (req, res) => {
+    res.send('404');
+  });
+
+app.use((err, req, res, next) => {
+    const defaultErr = {
+      log: 'Express error handler caught unknown middleware error',
+      status: 400,
+      message: { err: 'An error occurred' },
+    };
+    const errorObj = Object.assign(err, defaultErr);
+    console.log(errorObj.log);
+    const status = JSON.stringify(errorObj.status);
+    const response = JSON.stringify(errorObj.message);
+    return res.status(status).send(response);
+  });
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port: ${PORT}`);
+  });
+
+module.exports = app;
