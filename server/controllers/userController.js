eb58e60947859f2cb4bc8045133fd4091c68ce14
Simran Kaur <simran00kk@gmail.com>
2024-02-12 12:58:40 -0500

    signup middleware working and complete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const db = require('../config.js');
+const bcrypt = require('bcrypt');
 
 const userController = {};
 
@@ -8,6 +9,8 @@ userController.login = (req,res,next) => {
 
 userController.signup = (req,res,next) => {
     const email = req.body.email
+    const password = req.body.password 
+
     console.log('email: ',email)
     const userExistsQry = 
         `SELECT EXISTS (
@@ -17,17 +20,35 @@ userController.signup = (req,res,next) => {
         )`
     // const userExistsQry = 'SELECT * FROM users'
     const values = [email]
+    
     db.query(userExistsQry,values)
+        // check if email is alrdy being used
         .then(result => {
-            console.log(result)
-            console.log('L22')})
-            return next()
+            const exists = result.rows[0].exists
+
+            // if the email alrdy exists in the db return error message
+            if (exists) {
+                return res.status(400).json({ error: 'This email is already in use.' });
+
+            }
+
+            // otherwise, hash user's pw (this is an asyn func)
+            else {
+                return bcrypt.hash(password,10)
+            }  
+        })
+        // store user info (including hashed pw) in db
+        .then(passwordHash => {
+            const newUserQry = {
+                text: 'INSERT INTO users(email, password_hash) VALUES($1, $2)',
+                values: [email, passwordHash],
+              };
+            return db.query(newUserQry.text, newUserQry.values);
+        })
+        .then(()=> next())
         .catch(err => {
             return next(err)
-        })
-
-    
-    
+        })    
 }
 
 module.exports = userController
