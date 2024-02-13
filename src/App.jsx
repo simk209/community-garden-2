10406d2bb5d3dbdd2743eac6d3127441ecbde85c
Simran Kaur <simran00kk@gmail.com>
2024-02-12 19:06:07 -0500

    initial changes to frontend

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,28 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+// import styles from ./app.module.css
 
 function App() {
   const [count, setCount] = useState(0)
-
+const onLogin = async (email, password) => {
+try {
+  const response = await fetch('http://localhost:5000/login', {
+    method: 'POST', 
+    credentials: 'include', 
+    headers: {
+'Content-Type' : 'application/json'
+    }, 
+    body: JSON.stringify({
+      email, 
+      password,
+    })
+  })
+}
+catch(error) {
+  console.log('error', error)
+}
+}
   return (
     <>
       <div>
