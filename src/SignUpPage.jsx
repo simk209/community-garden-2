ed484998848ae964b79b424131319e2734719dc9
Simran Kaur <simran00kk@gmail.com>
2024-02-21 16:07:59 -0500

    added basic signup form

--- src/SignUpPage.jsx
+++ src/SignUpPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import {useState} from 'react'
 
 function SignUpPage() {
+
   return (
     <div>
       <h2>Sign Up</h2>
       {/* Include your signup form here */}
+      {/* you'll need an onsuvmit in the form */}
+      <form>
+        <div>
+        <label> Name:</label>
+        <input type="text" name="name"/>
+        </div>
+
+        <div>
+        <label> Email:</label>
+        <input type="text" name="email"/>
+        </div>
+        
+        <div>
+        <label> Password:</label>
+        <input type="text" name = "password" />
+        </div>
+
+      </form>
     </div>
   );
 }
