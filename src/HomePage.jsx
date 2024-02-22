6964486e3e418fc3a88cfa178ef33f3175d91581
Simran Kaur <simran00kk@gmail.com>
2024-02-22 16:18:38 -0500

    removed root styling from app css and adjusted styling on homepage by adding another flexbox

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -7,9 +7,13 @@ function HomePage() {
     <div className='body-container'>
       <div className='container'>
         <h1>Welcome to our Community Garden! </h1>
-        <p>Check resource availability, reserve what you need, and contribute to the garden!</p>
+        <div className='description'>
+        Check resource availability, reserve what you need, and contribute to the garden!
+        </div>
+        <div className='links'>
         <Link to="/signup" className='button'>Sign Up</Link>
         <Link to="/login" className = 'button'>Log In</Link>
+        </div>
       </div>
     </div>
   );
